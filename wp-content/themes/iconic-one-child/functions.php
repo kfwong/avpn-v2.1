0b0b2b9b881be562ade3e502d2aa2fbf9cb039a5
kfwong <kfwong@zoho.com>
2014-08-04 22:16:45 +0800

    Added functions.php to child theme.

---
+++ wp-content/themes/iconic-one-child/functions.php
@@ -0,0 +1,36 @@
+<?php
+
+## single-organisation.php adding full-width class to fill up sidebar space
+add_action( 'body_class', 'single_organisation_css');
+function single_organisation_css( $classes ) {
+  global $post;
+  if ( 'organisation' == $post->post_type ){
+  	$classes[] = 'full-width';
+  }
+  return $classes;
+}
+
+## single-investment-showcase.php adding full-width class to fill up sidebar space
+add_action( 'body_class', 'single_investment_showcase_css');
+function single_investment_showcase_css( $classes ) {
+  global $post;
+  if ( 'investment-showcase' == $post->post_type ){
+  	$classes[] = 'full-width';
+  }
+  return $classes;
+}
+
+function admin_acf_styles()
+{
+
+	wp_enqueue_style( 'css-select2', 'http://cdnjs.cloudflare.com/ajax/libs/select2/3.5.0/select2.min.css');
+ 
+    wp_enqueue_script( 'js-select2', 'http://cdnjs.cloudflare.com/ajax/libs/select2/3.5.0/select2.min.js' );
+
+    wp_enqueue_script( 'js-child', dirname( get_bloginfo('stylesheet_url')) . '/js/child.js' );
+
+}
+add_action( 'admin_enqueue_scripts', 'admin_acf_styles' );
+
+?>
+
---
+++ wp-content/themes/iconic-one-child/js/child.js
@@ -0,0 +1,3 @@
+$(document).ready(function() { 
+	$(".field_type-select select").select2({"width": "100%"});
+});
